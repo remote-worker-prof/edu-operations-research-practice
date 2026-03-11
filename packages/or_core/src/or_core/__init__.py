037653071d4a139c01980d3777964e9ddd3b8b52
Dmitry Vlasov <remote.worker.prof@gmail.com>
2026-03-11 11:02:31 +0300

    bd sync: 2026-03-11 11:02:31

--- packages/or_core/src/or_core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Operations Research core package for the educational demo."""
-
-from .pipeline import ORPipeline
-from .scenario import ScenarioBuilder
-
-__all__ = ["ORPipeline", "ScenarioBuilder"]
